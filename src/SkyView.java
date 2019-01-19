377e0239cabf0b9d045c60be1602deffc1f995d8
lostkid456 <jzhao7688@bths.edu>
2019-01-18 20:15:42 -0500

    LOLXD

--- src/SkyView.java
+++ src/SkyView.java
@@ -1,31 +1,43 @@
 public class SkyView {
     private double[][] view;
 
-    public SkyView(int numRows,int numCols,double[] scanned) {
-        int currPos = 0;
-        this.view = new double[numRows][numCols];
-        for(int i=0;i<numRows;i++){
-            if(i%2==0){
-                for(int j=0;j<numCols;j++){
-                    scanned[currPos]=view[i][j];
+    public SkyView(int numRows, int numCols, double[] scanned)
+    {
+        this.view= new double[numRows][numCols];
+        int currentPos=0;
+        int currentCol=0;
+
+        for(int i=0; i<numRows; i++)
+        {
+            if(currentCol%2!=0) {
+                for (int j = view[i].length-1; j >= 0; j--) {
+                    view[i][j] = scanned[currentPos];
+                    currentPos++;
                 }
-            } else {
-                for(int j=numCols;j<0;j--){
-                    scanned[currPos]=view[i][j];
+            }else{
+                for (int j = 0; j < view[i].length; j++) {
+                    view[i][j] = scanned[currentPos];
+                    currentPos++;
                 }
+            }
+            if(currentPos==scanned.length)
+            {
+                break;
             }
         }
     }
 
+
     public double getAverage(int startRow,int endRow, int startCol, int endCol) {
-        int sum = 0;
-        int a=endRow-startRow;
-        int b=endCol-startCol;
-        for (int i = startRow; i < endRow; i++) {
-            for (int j = startCol; j < endCol; j++) {
+        double sum = 0;
+        int a=endRow-startRow+1;
+        int b=endCol-startCol+1;
+        for (int i = startRow; i <=endRow; i++) {
+            for (int j = startCol; j <=endCol; j++) {
                 sum += view[i][j];
             }
         }
         return sum/(a*b);
     }
+
 }
